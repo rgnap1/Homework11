7e5e083ac37590c8853ce08310e7e4a828e3efcd
rgnap <osovskaya_1993@mail.ru>
2022-12-08 23:31:12 +0300

    Finish task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(" task 1 ");
+        int nowYear = 2022;
+     printLeapYearResult(nowYear);
+
+        System.out.println(" task 2 ");
+        int numberOS = 0;
+        int currentYear = LocalDate.now().getYear();
+        printOS(numberOS,currentYear);
+
+
+
     }
+
+    private static int printLeapYearResult ( int year ){
+        if (year % 4 == 0){
+            System.out.println(year + " високосный год.");
+        } else {
+            System.out.println(year + " не високосный год.");
+        }
+        return year;
+    }
+
+     private static int printOS (int number, int year){
+        if ( number == 0 && year < 2022 ){
+            System.out.println("Установите lite- версию OS для IOS");
+        } else if (number==1 && year < 2022){
+            System.out.println("Установите lite- версию OS для Android");
+        }else if ( number == 0 && year >=2022){
+            System.out.println("Установите стандартную версию OS для IOS");
+        }else if ( number == 1 && year >= 2022){
+            System.out.println("Установите стандартную версию OS для Android");
+        } else {
+            System.out.println("Введите корректные данные!");
+        }
+        return number;
+     }
+
+
 }
